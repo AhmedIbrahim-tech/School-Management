6aa713c939bd39f57e3dfc0bd81cc072916b010b
Ahmed Ibrahim <ebrahema89859@gmail.com>
2023-06-18 22:51:58 +0300

    Add project files.

---
+++ Data/Entities/DepartmetSubject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+public class DepartmetSubject
+{
+    [Key]
+    public int DeptSubID { get; set; }
+    public int DID { get; set; }
+    public int SubID { get; set; }
+
+    [ForeignKey("DID")]
+    public virtual Department Department { get; set; }
+
+    [ForeignKey("SubID")]
+    public virtual Subjects Subjects { get; set; }
+}
---
+++ Data/Entities/Subjects.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class Subjects
+{
+    public Subjects()
+    {
+        StudentsSubjects = new HashSet<StudentSubject>();
+        DepartmetsSubjects = new HashSet<DepartmetSubject>();
+    }
+    [Key]
+    public int SubID { get; set; }
+    [StringLength(500)]
+    public string SubjectName { get; set; }
+    public DateTime Period { get; set; }
+    public virtual ICollection<StudentSubject> StudentsSubjects { get; set; }
+    public virtual ICollection<DepartmetSubject> DepartmetsSubjects { get; set; }
+}
